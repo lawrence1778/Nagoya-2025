cd6071d789f50ca2dce4d60ca8738aeccd497b79
lawrence1778 <lawrence1778@gmail.com>
2025-11-24 23:29:13 +0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,17 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "名古屋親子六日遊",
-  description: "2024名古屋家庭旅遊行程表",
+  title: "Nagoya-2025", // 瀏覽器分頁標題
+  description: "2025名古屋家庭旅遊行程表",
   manifest: "/manifest.json",
+  icons: {
+    icon: '/icon.png', // 一般瀏覽器圖示
+    apple: '/icon.png', // iPhone 桌面圖示 (關鍵設定)
+  },
   appleWebApp: {
     capable: true,
     statusBarStyle: "black-translucent",
-    title: "Nagoya24",
+    title: "Nagoya-2025", // iPhone 加入主畫面時的預設名稱
   },
 };
 
